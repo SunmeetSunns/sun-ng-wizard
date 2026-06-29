06d2e5c4bad8fa33cf8aee9e029134a2811b3f4f
SunmeetSunns <kaursunmeet624@gmail.com>
2026-06-29 10:07:29 +0530

    Added code for scss states and added all code for library demo app

--- projects/cats-wizard/src/app/components/dummy-comp-1/dummy-comp-1.ts
+++ projects/cats-wizard/src/app/components/dummy-comp-1/dummy-comp-1.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dummy-comp-1',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dummy-comp-1.html',
   styleUrl: './dummy-comp-1.scss',
 })
 export class DummyComp1 {
-
+  selectedMethod: any;
+  constructor() {}
+  @Input() contextId: string | undefined;
+  wizardId: any = 'configure-product';
+  async processAndGoToNextStep(): Promise<void> {}
+  methods = [
+    {
+      title: 'Select From Template',
+      desc: 'Proceed with the pre-built template to configure the product. ',
+      img: 'assets/common/instructions_img.svg',
+      disabled: false,
+    },
+    {
+      title: 'Configure Manually',
+      desc: 'Proceed manually to configure the product.  ',
+      img: 'assets/common/manually_img.svg',
+      disabled: false,
+    },
+  ];
+  goToPrevStep(): void {}
+  selectMethod(index: number) {
+    if (!this.methods[index].disabled) {
+      this.selectedMethod = index;
+    }
+  }
 }
